859180a94e99b89ba6e1d7facb15466d66c4b4a4
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-12-24 20:19:05 +0300

    add dummy chart data

--- dash/admin/main.js
+++ dash/admin/main.js
@@ -115,3 +115,64 @@ function respondLeave() {
             $('#respondLeave').modal('hide')
         })
 }
+
+// charts
+charts = {
+
+    initDashboardPageCharts: function() {
+
+        var dataPreferences = {
+            series: [
+                [25, 30, 20, 25]
+            ]
+        };
+
+        var optionsPreferences = {
+            donut: true,
+            donutWidth: 40,
+            startAngle: 0,
+            total: 100,
+            showLabel: false,
+            axisX: {
+                showGrid: false
+            }
+        };
+
+        Chartist.Pie('#chartPreferences', dataPreferences, optionsPreferences);
+
+        Chartist.Pie('#chartPreferences', {
+            labels: ['53%', '36%', '11%'],
+            series: [53, 36, 11]
+        });
+
+        var data = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            series: [
+                [12, 3, 0, 0, 3, 5, 6, 0, 8, 0, 0, 5],
+                [2, 2, 0, 0, 3, 3, 0, 4, 1, 0, 6, 5]
+            ]
+        };
+
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: false
+            },
+            height: "245px"
+        };
+
+        var responsiveOptions = [
+            ['screen and (max-width: 640px)', {
+                seriesBarDistance: 5,
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                        return value[0];
+                    }
+                }
+            }]
+        ];
+
+        var chartActivity = Chartist.Bar('#chartActivity', data, options, responsiveOptions);
+
+    }
+}
